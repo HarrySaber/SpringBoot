dcc17c7cb46bc182526fd868761168dacff5b5ae
D.jin <jindiao1993@qq.com>
2018-03-15 15:42:35 +0800

    database

--- src/main/java/com/example/application/Application.java
+++ src/main/java/com/example/application/Application.java
@@ -9,12 +9,11 @@ import org.springframework.context.annotation.ComponentScan;
  * @author Jin.D
  * @date: 2018年3月15日 下午12:29:20
  */
+@SpringBootApplication
 @ComponentScan("com.example.controller")
 @MapperScan("com.example.mapper")
-@SpringBootApplication
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
-	//a
 }
